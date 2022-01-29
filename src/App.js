69b0f0192deceb22eff2b08baf66f5402ebcb263
RiyaSantoki <riyasantoki28@gmail.com>
2022-01-29 10:58:54 +0530

    Portifolio app in ReactJs
    Portifolio app in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {displayBio: false}
+
+
+
+    toggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+    
+
+    render(){
+        return(
+
+            <div>
+                <img src={profile} alt='profile' className='profile' />
+                <h1>Hello!</h1>
+                <p>My name is Riya. I'm software engineer.</p>
+                <p>I'm always looking forward to working on meningful Projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in San Feancisco ,and code every day. </p>
+                            <p>My favorite language is javaScript, and i think React.js is awesome.</p>
+                            <p>Besides coding, I also love Music!</p>
+                            <button type="button" className="btn btn-link" onClick={this.toggleDisplayBio}>Less More</button>
+                        </div>
+                    ) : (
+                        <div>
+                             <button type="button" className="btn btn-link" onClick={this.toggleDisplayBio}>Read More</button>
+                        </div>
+                    )
+                }
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
+            </div>
+        )
+    }
 }
 
 export default App;
